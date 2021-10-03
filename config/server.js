a25452270c63ad3fa2882a4393cf8b14fd04d7b6
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-03 04:53:31 -0500

    Initialized backend and added strapi/cloudinary manage package

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d66fdd457e37103ddd646d1bd6146ac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81d93847-1f1d-4ff5-b30e-47eb2a82ca2a'
+};
