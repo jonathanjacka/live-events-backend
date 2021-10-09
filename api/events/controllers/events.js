ae42fc784843fab77547de59fceb93ece5305ef8
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-09 04:57:10 -0500

    Added routes /events/me to access individual user events

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+const { default: createStrapi } = require("strapi");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  //get logged in users
+  async me(context) {
+    const user = context.state.user;
+
+    if (!user) {
+      return context.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const data = await strapi.services.events.find({ user: user.id });
+
+    if (!data) {
+      return context.notFound();
+    }
+
+    return sanitizeEntity(data, { model: strapi.models.events });
+  },
+};
